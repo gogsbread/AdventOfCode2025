eb23767eec27e7944dd2705b8d9f75c6ea26add4
Antony Deepak Thomas <antonydeepak@gmail.com>
2025-12-13 23:27:40 -0800

    llm improvements

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -1,31 +1,34 @@
 use aoc_2024::*;
 use std::error;
 
-fn p1(input: &Vec<String>) -> usize {
-    let cum_v = input.iter().scan(50, |acc, line| {
-        let mut d = line[1..].parse::<i32>().unwrap();
-        if line.starts_with('L') {
-            d = 100 - d;
-        }
-        *acc = (*acc + d) % 100;
-        Some(*acc)
-    });
-    cum_v.filter(|v| *v == 0).count()
+fn p1<I, S>(input: I) -> usize
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<str>,
+{
+    input
+        .into_iter()
+        .scan(50, |acc, line| {
+            let line = line.as_ref();
+            let mut d = line[1..].parse::<i32>().unwrap();
+            if line.starts_with('L') {
+                d = 100 - d;
+            }
+            *acc = (*acc + d) % 100;
+            Some(*acc)
+        })
+        .filter(|v| *v == 0)
+        .count()
 }
 
 fn p2(input: &Vec<String>) -> usize {
-    let input = input
-        .iter()
-        .flat_map(|line| {
-            let d = line[1..].parse::<i32>().unwrap();
-            if line.starts_with('L') {
-                (0..d).map(|_| "L1".to_string()).collect::<Vec<_>>()
-            } else {
-                (0..d).map(|_| "R1".to_string()).collect::<Vec<_>>()
-            }
-        })
-        .collect::<Vec<String>>();
-    p1(&input)
+    let iter = input.iter().flat_map(|line| {
+        let line = line.as_str();
+        let d = line[1..].parse::<usize>().unwrap();
+        let dir = if line.starts_with('L') { "L1" } else { "R1" };
+        std::iter::repeat(dir).take(d).map(|s| s.to_string())
+    });
+    p1(iter)
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
