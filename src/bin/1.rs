2e4832d27832ab8af40ddcd432b669f850a4c91c
Antony Deepak Thomas <antonydeepak@gmail.com>
2025-12-13 23:02:13 -0800

    1

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -1,7 +1,36 @@
 use aoc_2024::*;
 use std::error;
 
+fn p1(input: &Vec<String>) -> usize {
+    let cum_v = input.iter().scan(50, |acc, line| {
+        let mut d = line[1..].parse::<i32>().unwrap();
+        if line.starts_with('L') {
+            d = 100 - d;
+        }
+        *acc = (*acc + d) % 100;
+        Some(*acc)
+    });
+    cum_v.filter(|v| *v == 0).count()
+}
+
+fn p2(input: &Vec<String>) -> usize {
+    let input = input
+        .iter()
+        .flat_map(|line| {
+            let d = line[1..].parse::<i32>().unwrap();
+            if line.starts_with('L') {
+                (0..d).map(|_| "L1".to_string()).collect::<Vec<_>>()
+            } else {
+                (0..d).map(|_| "R1".to_string()).collect::<Vec<_>>()
+            }
+        })
+        .collect::<Vec<String>>();
+    p1(&input)
+}
+
 fn main() -> Result<(), Box<dyn error::Error>> {
-    write_out(read_in()?);
+    let input = read_in()?;
+    write_out(p1(&input));
+    write_out(p2(&input));
     Ok(())
 }
